f36f9af397c4d8793d6f54056b789cc98d0d87c2
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-10 21:47:37 -0400

    Initial Commit

---
+++ iCoffee/iCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCoffeeApp.swift
+//  iCoffee
+//
+//  Created by Marcelo Amaral Alves on 2023-10-10.
+//
+
+import SwiftUI
+
+@main
+struct iCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
